949a6ab57312563ff8df04bae8b01e9e8e073069
Eundongdong <jk4373@sookmyung.ac.kr>
2024-06-11 13:19:52 +0900

    feat : 월 변경 기능 구현
    - 월 변경 함수 : nextMonthList, previousMonthList
    - 날짜 형식 변경 함수 : formatDate

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,7 +1,10 @@
 <template lang="">
     <div>
         <div>필터</div>
-        <div>날짜</div>
+        <div>
+            <a href="#" class="previous round" @click="previousMonthList">&#8249;</a>
+            {{currentDate.year}}-{{currentDate.month}}
+            <a href="#" class="`next round" @click="nextMonthList">&#8250;</a></div>
         <div>리스트
             <table>
                 <thead>
@@ -21,27 +24,74 @@
                         <td>{{i.date}}</td>
                         <td>{{i.amount}}</td>
                         <td>{{i.asset}}</td>
-                        <td>{{i.content}}</td>
+                        <!-- <td>{{i.content}}</td> -->
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
+    {{currentDate}}
 </template>
 <script>
+
 import axios from 'axios';
-import {onMounted,reactive} from 'vue';
+import {computed, onMounted,reactive, ref} from 'vue';
 export default {
     name : "List",
     setup(){
+        
         let lists = reactive([])
-        onMounted(async() => {
-            
-            const response = await axios.get('http://localhost:3001/data')
-            console.log(response)
-            Object.assign(lists,response.data)
+        const currentDate = reactive({
+            year : 2024,
+            month : 1
         })
-        return {lists}
+        onMounted(async() => {
+            currentDate.year = new Date().getFullYear()
+            currentDate.month = new Date().getMonth() + 1
+            await getCurrentMonthList()
+            // lists.filter(function(date){
+            //     console.log(date)
+            //     console.log('--------')
+            // })
+        })
+        
+        //내역 가져오기
+        const getCurrentMonthList = async()=>{
+            const response = await axios.get('http://localhost:3001/data')
+            response.data.map(item=>item.date= formatDate(item.date))
+            Object.assign(lists,response.data)
+        }
+        //formDate : 날짜를 'YYYY-MM-DD'형식으로 변환하는 함수
+        const formatDate = (date) => {
+            const d = new Date(date)
+            const year = d.getFullYear()
+            const month = String(d.getMonth() + 1).padStart(2, '0')
+            const day = String(d.getDate()).padStart(2, '0')
+            return `${year}-${month}-${day}`
+        }
+
+        const filteredTest = computed(()=>{
+            return lists.filter(i =>i.date === currentDate.month)
+        })
+
+        // 다음 월의 내역 가지고오기
+        const nextMonthList = (event) =>{
+            if(currentDate.month >11){
+                currentDate.month = 1
+                currentDate.year++
+            }else{
+                currentDate.month++
+            }
+        }
+        const previousMonthList = (event) =>{
+            if(currentDate.month <2){
+                currentDate.month = 12
+                currentDate.year--
+            }else{
+                currentDate.month--
+            }
+        }
+        return {lists,currentDate,nextMonthList,previousMonthList}
     }
 }
 </script>
