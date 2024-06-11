19b2f36098596531ad7d4599fd067ceb6d2e93d2
Eundongdong <jk4373@sookmyung.ac.kr>
2024-06-11 10:21:08 +0900

    feat : List 컴포넌트 추가, List 라우터 설정
    - List 컴포넌트 서버 연결
    - /list 라우터 추가
    - json-server, axios, bootstrap 설치
    - App.vue 수정 및 <routerView>태그 추가

---
+++ src/pages/List.vue
@@ -0,0 +1,50 @@
+<template lang="">
+    <div>
+        <div>필터</div>
+        <div>날짜</div>
+        <div>리스트
+            <table>
+                <thead>
+                    <tr>
+                        <td>날짜</td>
+                        <td>금액</td>
+                        <td>유형</td>
+                        <td>분류</td>
+                        <td>자산</td>
+                        <td>내용</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="i in lists" :key="i.id" :class="`tr ${i.id}`">
+                        <td>{{i.id}}</td>
+                        <td>{{i.type}}</td>
+                        <td>{{i.date}}</td>
+                        <td>{{i.amount}}</td>
+                        <td>{{i.asset}}</td>
+                        <td>{{i.content}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+import {onMounted,reactive} from 'vue';
+export default {
+    name : "List",
+    setup(){
+        let lists = reactive([])
+        onMounted(async() => {
+            
+            const response = await axios.get('http://localhost:3001/data')
+            console.log(response)
+            Object.assign(lists,response.data)
+        })
+        return {lists}
+    }
+}
+</script>
+<style lang="">
+    
+</style>
