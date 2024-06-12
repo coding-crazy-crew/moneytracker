b34317b39b304e8b36f6d97c59079e40b1c27c04
yeon-jae <lisanamyj99@gmail.com>
2024-06-12 17:47:54 +0900

    chore:캘린더 수정
    merge index.js간단한 수정

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,10 @@ import Home from '@/pages/Home.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name: "home" },
     {path: "/about",name: "about"},
     {path: "/calendar", name: "calendar", component: Calendar},
     {path: '/list',component: List},
-    {path: '/register', name: 'register', component: Register}
+    {path: '/register', name: 'register', component: Register},
     { path: '/', name: 'home', component: Home},
     { path: '/list',component: List},
   ]
