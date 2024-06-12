18da56d3ccefa3b20b0b2fa4944af750fac4e0ba
kjy <gimjuyeong0620@gmail.com>
2024-06-12 11:00:25 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Header from './components/Header.vue';
-import ToRegisterButton from './components/ToRegisterButton.vue';
 </script>
 <template>
   <Header />
   <router-view></router-view>
-  <ToRegisterButton />
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Main from '@/pages/Main.vue'
 import Register from '@/pages/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'main', component: Main },
     { path: '/register', name: 'register', component: Register}
   ]
 })
