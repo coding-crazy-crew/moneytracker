0b0b254347adc24c1414034fe52db073e4f2e08d
Eundongdong <jk4373@sookmyung.ac.kr>
2024-06-13 13:17:10 +0900

    chore : 변수 수정

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,42 +1,53 @@
 <template lang="">
-    <div>
-        <form @submit.prevent="loginFormSubmitHandler">
+    <form @submit.prevent="loginFormSubmitHandler">
         <label for="i">아이디:</label>
         <input type="text" name="id" id="i" required  v-model="id"><br>
         <label for="p">비밀번호:</label>
         <input type="password" name="pwd" id="p" required v-model="pwd"><br>
         <input type="checkbox" v-model="checked">아이디저장
-        <button>로그인</button> <!--<button type="submit">과 같음-->
+        <button>로그인</button>
     </form>
-    </div>
 </template>
 <script>
+import { useLoginInfoStore } from '@/stores/LoginInfo';
 import axios from 'axios';
+import {ref} from 'vue'
 export default {
-    name: "Login",
+    name: "SignIn",
     setup() {
         let id = ref('')
         let pwd = ref('')
         let checked = ref(true)
-        //----form객체에서 submit이벤트가 발생했을 때 할 일 START----
-        // function loginFormSubmitHandler() {
+        const store = useLoginInfoStore()
+
         const loginFormSubmitHandler = async()=>{
-            const url = 'http://localhost:3000/login'
+            const url = 'http://localhost:3000/user'
             const data = {
                 id: id.value,
                 pwd: pwd.value
             }
             try{
-                const response = await axios.post(url,data, {"Content-Type": "application/json"})
-            console.log(response)
+                const response = await axios.get(url+"?userId="+data.id+"&pwd="+data.pwd, {"Content-Type": "application/json"})
+                console.log(response.data)
+                if (response.data){
+                    console.log("로그인 성공!")
+                    store.logIn(data.id)
+                }else{
+                    alert("아이디 혹은 비밀번호를 다시 입력하세요.")
+                    
+                    console.log("로그인 실패!")
+                }
             }catch(err){
                 console.log(err.message)
             }
         }
+
+        //id 저장
+
         return {id,  pwd, loginFormSubmitHandler}
     }
 }
 </script>
 <style lang="">
-    
+
 </style>
--- src/stores/LoginInfo.js
+++ src/stores/LoginInfo.js
@@ -1,19 +1,26 @@
-import { co } from '@fullcalendar/core/internal-common';
 import {defineStore} from 'pinia'
 import { computed } from 'vue';
 
 export const useLoginInfoStore = defineStore("LoginInfo",()=>{
-    const userId = ref(0); //로그인 안 한 상태
+    const userId = ref('0'); //로그인 안 한 상태
+    const storedUserId = ref('')
 
     const logIn = ((id)=>{
         userId.value = id
         console.log(id+"로 로그인 성공")
     })
 
+    const rememberUserId = ((id)=>{
+        storedUserId.value = id
+        console.log(id+"로 로그인 성공")
+    })
+
+    
+
     const getLoginId =computed(()=>userId.value)
 
     const logOut = ((id)=>{
-        userId.value = 0
+        userId.value = '0'
         console.log("로그아웃 성공")
     })
 
