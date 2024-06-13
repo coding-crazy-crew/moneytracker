0176445d6c7fb8c65f73ca9e750751beff0691bc
Eundongdong <jk4373@sookmyung.ac.kr>
2024-06-13 10:56:29 +0900

    feat : SignIn component 라우터 등록

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,11 +1,40 @@
 <template lang="">
     <div>
-        
+        <form @submit.prevent="loginFormSubmitHandler">
+        <label for="i">아이디:</label>
+        <input type="text" name="id" id="i" required  v-model="id"><br>
+        <label for="p">비밀번호:</label>
+        <input type="password" name="pwd" id="p" required v-model="pwd"><br>
+        <input type="checkbox" v-model="checked">아이디저장
+        <button>로그인</button> <!--<button type="submit">과 같음-->
+    </form>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
-    
+    name: "Login",
+    setup() {
+        let id = ref('')
+        let pwd = ref('')
+        let checked = ref(true)
+        //----form객체에서 submit이벤트가 발생했을 때 할 일 START----
+        // function loginFormSubmitHandler() {
+        const loginFormSubmitHandler = async()=>{
+            const url = 'http://localhost:3000/login'
+            const data = {
+                id: id.value,
+                pwd: pwd.value
+            }
+            try{
+                const response = await axios.post(url,data, {"Content-Type": "application/json"})
+            console.log(response)
+            }catch(err){
+                console.log(err.message)
+            }
+        }
+        return {id,  pwd, loginFormSubmitHandler}
+    }
 }
 </script>
 <style lang="">
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,18 @@ import Calendar from "@/pages/Calendar.vue";
 import List from '@/pages/List.vue';import SignUp from '@/pages/SignUp.vue'
 import Register from "@/pages/Register.vue";
 import Home from '@/pages/Home.vue';
+import SignIn from "@/pages/SignIn.vue";
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: "/about",name: "about"},
-    {path: "/calendar", name: "calendar", component: Calendar},
+    {path: '/', name: 'home', component: Home},
     {path: '/list',component: List},
+    {path: "/calendar", name: "calendar", component: Calendar},
     {path: '/register', name: 'register', component: Register},
-    { path: '/', name: 'home', component: Home},
-    { path: '/list',component: List},
-    {path: '/signup', component: SignUp}
+    {path: '/signup', component: SignUp},
+    {path: '/signin', component: SignIn}
   ]
 })
 
